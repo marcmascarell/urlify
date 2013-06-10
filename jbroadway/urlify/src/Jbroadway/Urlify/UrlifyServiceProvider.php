295c32f9dfcc58df7dabe4e13e03d6d85f36a872
marc <marcmascarell2gmail.com>
2013-06-10 17:18:45 +0200

    intial

---
+++ jbroadway/urlify/src/Jbroadway/Urlify/UrlifyServiceProvider.php
@@ -0,0 +1,47 @@
+<?php namespace Jbroadway\Urlify;
+
+use Illuminate\Support\ServiceProvider;
+use Jbroadway\URLify;
+
+class UrlifyServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('jbroadway/urlify');
+
+        include 'URLify.php';
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
